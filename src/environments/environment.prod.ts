707c987f90165fd190482a7254c151259a6ceaa6
Mark Csoma <84617390+midorisol@users.noreply.github.com>
2023-01-09 22:35:55 +0100

    First part of API request

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/',
+  XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue:'open-weather13.p.rapidapi.com',
+  XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue:'fac3edcbb8msh3e7c5f68ee8943ap1141cejsn29a89c2d3b06',
+};
